3508dc39f33f8ee800afd8d8add63fa56a2b4c34
nicolaszsch <nzambran@uc.cl>
2025-12-23 23:30:01 -0300

    Módulo de funciones actualizado.

--- sm_funciones.py
+++ sm_funciones.py
@@ -1,3 +1,24 @@
+import pandas as pd
+from datetime import datetime
+
+def leer_csv(data_dir):
+    """
+    Lee y retorna la información contenida en el CSV que se encuentra 
+    en data_dir.
+
+    Parámetros:
+    data_dir (str): Dirección con la ubicación del archivo CSV del cual
+        se quiere obtener la información.
+
+    Retorna:
+    data (pd.DataFrame): Datos con la información contenida en el
+        archivo CSV.
+    """
+    data = pd.read_csv(data_dir, header=0, parse_dates=False, index_col=0,
+                    sep=',')
+    return data
+
+
 def leer_txt(location):
     """
     Retorna una lista en la que sus elementos son las líneas del 
@@ -7,3 +28,10 @@ def leer_txt(location):
     datos = read_datos.readlines()
     read_datos.close()
     return datos
+
+def transformar_fecha(text):
+    """ Transforma el string con la fecha en un datetime"""
+    ano = int(text[:4])
+    mes = int(text[5:7])
+    dia = int(text[8:])
+    return datetime(ano, mes, dia)
